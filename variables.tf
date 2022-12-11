333de54568fde666be57528863f2b9d38f529e67
serge <humphreynkongho@gmail.com>
2022-12-11 14:18:10 -0600

    terraform code

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0b0dcb5067f052a63"
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t2.micro"
+}
+variable "keypair" {
+  type    = string
+  default = "awskeypair"
+}
+variable "name" {
+  type    = string
+  default = "dev-server"
+}
